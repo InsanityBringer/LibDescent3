78758b7d7325ddddcccbb285cd09dd2510ae8f4f
InsanityBringer <1504999+InsanityBringer@users.noreply.github.com>
2021-03-29 22:15:37 -0500

    Fix missing headers and add RGBA8->RGBA4 tests

---
+++ LibDescent3.Tests/ColorConversionTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace LibDescent3.Tests
+{
+    class ColorConversionTests
+    {
+        [Test]
+        public void TestColorConversion()
+        {
+            Assert.AreEqual(0xFFFF, ColorUtils.Convert32BitToRGBA4444(0xFFFFFFFF));
+            Assert.AreEqual(0xF777, ColorUtils.Convert32BitToRGBA4444(0xFF7F7F7F));
+            Assert.AreEqual(0x8000, ColorUtils.Convert32BitToRGBA4444(0x80000000));
+        }
+    }
+}
